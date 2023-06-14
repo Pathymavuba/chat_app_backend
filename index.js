8f1db4d87706d8eba7112eb389fafcb6b25af2d5
pathymavuba <mavubapathy@gmail.com>
2023-06-14 14:38:37 +0100

    get users connected after decconection

--- index.js
+++ index.js
@@ -17,20 +17,31 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 //create socket server
 const io = socketio(server, { cors: { origin: "http://localhost:3000" } });
-
+let users = [];
 io.on("connection", (socket) => {
   console.log(`${socket.id} user  is connected`);
 
   //listner for event messages
-  socket.on("message",(data)=>{
+  socket.on("message", (data) => {
     // console.log(data)
     //send message to all Clients connected
-    io.emit("MessageResponse",data)
-  })
+    io.emit("MessageResponse", data);
+  });
 
-  socket.on("deconnection", function () {
+  // listen when a new newser join chat
+  socket.on("newUser", (data) => {
+    users.push(data);
+    // console.log(data)
+    io.emit("NewUserResponse", users);
+  });
+
+  socket.on("disconnect", function () {
     console.log("a user is disconnected");
+
+    //update list of connected users
+    users = users.filter((user) => user.socketID !== socket.id);
+    //resent a new list
+    io.emit("NewUserResponse", users);
+    socket.disconnect();
   });
 });
-
-
